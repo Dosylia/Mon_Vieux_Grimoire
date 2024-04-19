5346b111e99aa0575a6128cee116ca762ef80441
Dosylia <emma.montbarbon@outlook.fr>
2024-04-19 10:34:26 +0200

    Added middleware Auth

--- routes/books.js
+++ routes/books.js
@@ -1,16 +1,17 @@
 const express = require ('express');
+const auth = require('auth')
 const router = express.Router();
 
 const booksCtrl = require('../controllers/books');
 
 
-router.get('/', booksCtrl.showAllBooks);
-router.get('/:id', booksCtrl.showBookById);
-router.get('/bestrating', booksCtrl.showAllBooks);
-router.post('/', booksCtrl.addBook);
-router.put('/:id', booksCtrl.updateBook);
-router.delete('/:id', booksCtrl.deleteBook);
-router.post('/:id/rating', booksCtrl.addBookRating);
+router.get('/', auth, booksCtrl.showAllBooks);
+router.get('/:id', auth, booksCtrl.showBookById);
+router.get('/bestrating', auth, booksCtrl.showAllBooks);
+router.post('/', auth, booksCtrl.addBook);
+router.put('/:id', auth, booksCtrl.updateBook);
+router.delete('/:id', auth, booksCtrl.deleteBook);
+router.post('/:id/rating', auth, booksCtrl.addBookRating);
 
 
 module.exports = router;
